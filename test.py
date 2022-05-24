2fad5bd13f84bd28bac3b05b64cb693339dbdeb8
Vo The Minh <18120211>
2022-05-24 22:57:08 +0700

    up

--- test.py
+++
@@ -1,9 +0,0 @@
-import logging
-logging.basicConfig(filename='/etc/error.log',
-                        level=logging.DEBUG,
-                        format='[%(asctime)s] %(levelname)s in app.py: %(message)s', datefmt='%m/%d/%y %H:%M:%S')
-
-logfile = logging.getLogger('')
-logfile.info('nothing')
-logfile.info('hahaha')
-logfile.log(msg='ANH', level=logging.DEBUG)
